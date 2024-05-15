e758646f6246a98f91a1f92a7d06ef89b94f4656
Nataliafg7 <127059274+Nataliafg7@users.noreply.github.com>
2024-05-15 06:57:14 -0500

    Update SecretConfig-sample.py

--- SecretConfig-sample.py
+++ SecretConfig-sample.py
@@ -9,4 +9,4 @@ PGDATABASE = "Ingrese el nombre de la base de datos"
 PGUSER = "Ingrese el usuario de la BD"
 PGPASSWORD = "Ingrese la contraseña"
 PGHOST = "Ingrese la dirección DNS o dirección IP del servidor"
-PGPORT = 5432 # El valor predeterminado es 5432, pero puedes cambiar
+PGPORT = 5432 # El valor predeterminado es 5432
