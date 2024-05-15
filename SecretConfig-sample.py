e7c2866afa62034ef23f7533d420e5c9c309e5e9
Nataliafg7 <127059274+Nataliafg7@users.noreply.github.com>
2024-05-15 06:56:52 -0500

    Add files via upload

---
+++ SecretConfig-sample.py
@@ -0,0 +1,12 @@
+"""
+
+Complete esta información y guarde este archivo como SecretConfig.py
+para poder ejecutar la aplicación
+
+"""
+
+PGDATABASE = "Ingrese el nombre de la base de datos"
+PGUSER = "Ingrese el usuario de la BD"
+PGPASSWORD = "Ingrese la contraseña"
+PGHOST = "Ingrese la dirección DNS o dirección IP del servidor"
+PGPORT = 5432 # El valor predeterminado es 5432, pero puedes cambiar
