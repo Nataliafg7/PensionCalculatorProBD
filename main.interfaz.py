5031b4fe48b18b7591df19abb1f1c627a65179e5
Nataliafg7 <127059274+Nataliafg7@users.noreply.github.com>
2024-05-15 21:57:11 -0500

    Delete main.interfaz.py

--- main.interfaz.py
+++
@@ -1,8 +0,0 @@
-from kivy.app import App
-
-import sys 
-sys.path.append("src")
-# Importa la clase PensionCalculatorApp desde el archivo pension_interface.py
-from vista.pension_interface import *
-if __name__ == '__main__':
-    PensionCalculatorApp().run()
